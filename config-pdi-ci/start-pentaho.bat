be702a682dd3263b811145dbbec9d78fae49a098
Beppe Raymaekers <beppe.raymaekers@gmail.com>
2019-05-14 10:58:07 +0200

    bat script updates

--- config-pdi-ci/start-pentaho.bat
+++ config-pdi-ci/start-pentaho.bat
@@ -1,4 +1,4 @@
-SET PENTAHO_ENV=config-pdi-local
+SET PENTAHO_ENV=config-pdi-ci
 SET PROJECT_NAME=sales_dwh
-CD %~dp0\..\..\framework\configuration
+CD %~dp0\..\..\%PROJECT_NAME%\framework\configuration
 CALL start-pentaho.bat %*
