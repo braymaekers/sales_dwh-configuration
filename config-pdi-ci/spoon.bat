057dd82c9bcff57fda6dff7790ecb22072a88123
Beppe Raymaekers <beppe.raymaekers@gmail.com>
2019-05-29 14:38:53 +0200

    updated config

--- config-pdi-ci/spoon.bat
+++ config-pdi-ci/spoon.bat
@@ -1,4 +1,4 @@
-SET PENTAHO_ENV=config-pdi-ci
+SET PENTAHO_ENV=config-pdi-local
 SET PROJECT_NAME=sales_dwh
 CD %~dp0\..\..\%PROJECT_NAME%\framework\configuration
 CALL spoon.bat
--- config-pdi-local/spoon.sh
+++ config-pdi-local/spoon.sh
@@ -2,12 +2,11 @@
 #PENTAHO_ENV = current folder name, eg. config-pdi-local
 #PROJECT_NAME = current project name, eg. sales_dwh
 
-
 export PENTAHO_ENV=$(basename $PWD)
 export PROJECT_NAME=$(basename $(dirname $PWD) -configuration) #removing -configuration from dir name
 
 echo "***" Setting PENTAHO_ENV to $PENTAHO_ENV "***"
 echo "***" Setting PROJECT_NAME to $PROJECT_NAME "***"
 
-cd "$PWD"/../../$PROJECT_NAME/framework/configuration
+cd "$PWD"/../../framework/configuration
 sh spoon.sh
