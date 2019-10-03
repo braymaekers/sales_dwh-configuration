527ec23b7c945cf49cabfac44045d43089355833
Beppe Raymaekers <beppe.raymaekers@gmail.com>
2019-10-03 11:11:24 +0200

    alignment of sh and bat scripts

--- config-pdi-local/spoon.bat
+++ config-pdi-local/spoon.bat
@@ -1,7 +1,13 @@
 @echo off
 
-REM Determine the PENTAHO_ENV configuration based on parent directory
-for %%a in ("%~dp0\.") do set "PENTAHO_ENV=%%~nxa"
+REM Determine the PROJECT_NAME and _ENV configuration based on parent directory
+for %%a in ("%~dp0\.") do set "PROJECT_NAME_TEMP=%%~nxa"
+set PROJECT_NAME=%PROJECT_NAME_TEMP:-configuration=%
+
+for %%a in ("%~dp0\.") do set "PROJECT_ENV=%%~nxa"
+
+ECHO *** Setting PROJECT_NAME to "%PROJECT_NAME%" ***
+ECHO *** Setting PROJECT_ENV to "%PROJECT_ENV%" ***
 
 REM BASE_CONFIG_DIR is used later in init.bat to call back to this config-dir
 set BASE_CONFIG_DIR=%CD%
